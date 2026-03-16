3761e470e74a50b32dced8ab71af8eea9cec47aa
Pabloku <paloga15@hotmail.com>
2026-03-16 22:36:21 +0100

    FEAT: Configure transparent system bars for edge-to-edge layout

--- app/src/main/java/com/pabloku/picalertsapp/MainActivity.kt
+++ app/src/main/java/com/pabloku/picalertsapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.pabloku.picalertsapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
@@ -36,7 +38,16 @@ import timber.log.Timber
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
         setContent {
             PicAlertsAppTheme {
                 val mainViewModel = hiltViewModel<MainViewModel>()
